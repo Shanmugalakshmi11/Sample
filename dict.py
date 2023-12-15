a7818885c6be6b847e178aff1e4fbbfead87103e
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2023-12-15 12:40:39 +0100

    implement dict

---
+++ dict.py
@@ -0,0 +1,12 @@
+# creating a dictionary
+country_capitals = {
+  "United States": "Washington D.C.", 
+  "Italy": "Rome", 
+  "England": "London"
+}
+
+# printing the dictionary
+print(country_capitals)
+
+
+
